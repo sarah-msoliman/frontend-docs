2d5fbc3a01acf4a9bbc85c1f79efa39cffcc5517
Sarah Soliman <sarah.soliman@elmenus.com>
2021-11-28 15:21:56 +0200

    Query each article.

--- src/templates/article-details.js
+++ src/templates/article-details.js
@@ -1,7 +1,31 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default function ArticleDetails() {
+import GeneralLayout from "../layouts/GeneralLayout"
+
+export default function ProjectDetails({ data }) {
+  const { html } = data.markdownRemark
+  const { title, icon, description, tag, slug } =
+    data.markdownRemark.frontmatter
+
   return (
-    <div>Hello</div>
+    <article>
+      <h2>{title}</h2>
+    </article>
   )
 }
+
+export const query = graphql`
+  query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        icon
+        description
+        tag
+        slug
+      }
+    }
+  }
+`
