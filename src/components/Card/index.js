c8112e9d138b596f20a7f37ca7246edea4c7039f
Sarah Soliman <sarah.soliman@elmenus.com>
2021-11-29 17:02:50 +0200

    Icon component.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,25 +1,15 @@
 import React from "react"
-import {
-  ClipboardListIcon,
-  BookOpenIcon,
-  CogIcon,
-} from "@heroicons/react/outline"
+
+import Icon from "../Icon"
 
 import * as styles from "../../styles/card.module.scss"
 
 export default function Card({ data }) {
-  const iconClassName = "h-5 w-5"
   return (
     <div className={`${styles.card} p-5`}>
       <h3 className="flex items-center font-bold">
         <span className="p-4 mr-2">
-          {data.icon === "ClipboardListIcon" ? (
-            <ClipboardListIcon className={iconClassName} />
-          ) : data.icon === "CogIcon" ? (
-            <CogIcon className={iconClassName} />
-          ) : (
-            <BookOpenIcon className={iconClassName} />
-          )}
+          <Icon icon={data.Icon} />
         </span>
         {data.title}
       </h3>
