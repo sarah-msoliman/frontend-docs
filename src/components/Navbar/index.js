913913f5730958f87043cb5f05267d9644b8d5fb
Sarah Soliman <sarah.soliman@elmenus.com>
2021-11-29 15:49:00 +0200

    Fixed logo img issue.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -8,7 +8,7 @@ export default function Navbar() {
     <nav className={`${styles.navbar} p-4 lg:p-8`}>
       <Link to="/">
         <h1 className={`${styles.navbar__logo} text-2xl font-bold`}>
-          <img src="logo.jpg" />
+          <img src="/logo.jpg" />
           Ninjas
         </h1>
       </Link>
