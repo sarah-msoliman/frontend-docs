47b1116dad872bee77b8a092e3f316068b3060b3
Sarah Soliman <sarah.soliman@elmenus.com>
2021-11-29 16:41:34 +0200

    Wrapped nav inside a header.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,13 +5,15 @@ import * as styles from "../../styles/navbar.module.scss"
 
 export default function Navbar() {
   return (
-    <nav className={`${styles.navbar} p-4 lg:p-8`}>
-      <Link to="/">
-        <h1 className={`${styles.navbar__logo} text-2xl font-bold`}>
-          <img src="/logo.jpg" />
-          Ninjas
-        </h1>
-      </Link>
-    </nav>
+    <header>
+      <nav className={`${styles.navbar} p-4 lg:p-8`}>
+        <Link to="/">
+          <h1 className={`${styles.navbar__logo} text-2xl font-bold`}>
+            <img src="/logo.jpg" />
+            Ninjas
+          </h1>
+        </Link>
+      </nav>
+    </header>
   )
 }
