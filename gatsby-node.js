172416311b3cb8cf0cdb8f8ee33b6eac06fc9a2b
Sarah Soliman <sarah.soliman@elmenus.com>
2022-02-07 16:25:51 +0200

    docs folder.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,23 @@
 const path = require(`path`);
+const fs = require("fs");
+
+exports.onPreInit = () => {
+  if (process.argv[2] === "build") {
+    fs.rmdirSync(path.join(__dirname, "docs"), { recursive: true })
+    fs.renameSync(
+      path.join(__dirname, "public"),
+      path.join(__dirname, "public_dev")
+    )
+  }
+}
+
+exports.onPostBuild = () => {
+  fs.renameSync(path.join(__dirname, "public"), path.join(__dirname, "docs"))
+  fs.renameSync(
+    path.join(__dirname, "public_dev"),
+    path.join(__dirname, "public")
+  )
+}
 
 exports.createPages = async ({ graphql, actions }) => {
   const { data } = await graphql(`
