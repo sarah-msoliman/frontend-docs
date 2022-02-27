c33b1e3083daf2f5ef42cd454a61a8ba39fc24c1
Sarah <sarah.soliman@elmenus.com>
2022-02-27 18:38:25 +0200

    fixed background color issue.

--- src/layouts/ArticleLayout/index.js
+++ src/layouts/ArticleLayout/index.js
@@ -4,7 +4,7 @@ import Navbar from "../../components/Navbar";
 
 export default function ArticleLayout({ children }) {
   return (
-    <div className="layout-container">
+    <div>
       <Navbar />
       <div className="w-w31p-8 md:px-0 md:py-16 narrow-container ">
         {children}
