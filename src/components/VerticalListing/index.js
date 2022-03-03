965400e335f603e6c958d1fad65d4371c08ea525
Sarah <sarah.soliman@elmenus.com>
2022-03-03 21:41:37 +0200

    Add title to horizontal listing.

--- src/components/VerticalListing/index.js
+++ src/components/VerticalListing/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import HorizontalCard from "../HorizontalCard";
 
-export default function VerticalListing({ data }) {
+export default function VerticalListing({ data, title }) {
   return (
     <div>
+      <h3 className="text-3xl font-bold mb-4">{title}</h3>
       {data.map((item, index) => (
         <HorizontalCard key={index} data={item.frontmatter} />
       ))}
